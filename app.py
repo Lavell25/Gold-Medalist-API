a3a6b0205df3022151718fd79d9809d9b03efc27
Kahlil F <kahlilf@Kahlils-MacBook-Pro.local>
2022-01-18 03:10:35 -0600

    installed dependencies

---
+++ app.py
@@ -0,0 +1,12 @@
+from itertools import count
+from wsgiref.util import request_uri
+from flask import Flask
+from flask import request
+from flask import jsonify
+from peewee import *
+
+from playhouse.shortcuts import model_to_dict, dict_to_model
+
+db = PostgresqlDatabase('athlete', user='postgres',
+                        password='', host='localhost', port=5432)
+
