c8e8f4bff84be89924b40e28e364d3d859803975
Kahlil F <kahlilf@Kahlils-MacBook-Pro.local>
2022-01-18 03:12:57 -0600

    added athletes

--- app.py
+++ app.py
@@ -27,3 +27,18 @@ class Athlete(BaseModel):
 db.connect()
 db.drop_tables([Athlete])
 db.create_tables([Athlete])
+
+athlete0 = Athlete(
+    name='Michael Phelps',
+    event='200m Butterfly',
+    place='First',
+    olympic_year='2004, 2008, 2012',
+    medal_recieved='Gold'
+)
+athlete1 = Athlete(
+    name='Usain Bolt',
+    event='100m',
+    place='First',
+    olympic_year='2008, 2012, 2016',
+    medal_recieved='Gold'
+)
